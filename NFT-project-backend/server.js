c1ee4e977971d07b5ec96999625ceab0fc9f90f7
Patrik Simandl <patrik.simandl@seznam.cz>
2025-11-15 16:50:30 +0100

    Create Setup

---
+++ NFT-project-backend/server.js
@@ -0,0 +1,14 @@
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req,res) =>{
+  console.log('GET request');
+  res.sendStatus(200);
+})
+
+
+app.listen(PORT, () => {
+  console.log('Server started at port',PORT);
+});
