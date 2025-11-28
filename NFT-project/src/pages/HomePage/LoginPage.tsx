910cd2c46587c1594f6815244b8e8f4a860fcbb7
Patrik Simandl <patrik.simandl@seznam.cz>
2025-11-28 22:59:52 +0100

    Finish menu add logo and start working on login page

--- NFT-project/src/pages/HomePage/LoginPage.tsx
+++ NFT-project/src/pages/HomePage/LoginPage.tsx
@@ -1,14 +1,28 @@
 export function LoginPage() {
   return (
-    <div className="login-page-container flex flex-col justify-center items-center bg-sky-200 p-[100px] rounded-[30px]">
-      <div className="font-semibold text-start w-full">Login</div>
-      <div className="flex gap-[10px]">
-        <input className="input-primary" placeholder="First name" />
-        <input className="input-primary" placeholder="Last name" />
+    <div className="login-page-container flex flex-col justify-center items-center bg-white shadow-lg p-[100px] rounded-[30px] w-[40%]">
+      <div className="font-semibold text-start w-full text-[22px]
+      pb-4">Log in to your account</div>
+      <div className="flex gap-[10px] w-full">
+        <div className="input-container">
+          <input className="input-primary" />
+          <label className="input-label">First name</label>
+        </div>
+        <div className="input-container">
+          <input className="input-primary" />
+          <label className="input-label ">Last name</label>
+        </div>
       </div>
-      <input className="input-primary" placeholder="Email" />
-      <input className="input-primary" placeholder="Password" />
-      <div>
+      <div className="input-container">
+        <input className="input-primary" />
+        <label className="input-label">Email</label>
+      </div>
+      <div className="input-container">
+        <input className="input-primary" type="password" />
+        <label className="input-label">Password</label>
+      </div>
+      <button className="button-primary w-full p-2.5 rounded-[8px] mt-[12px]">Log in</button>
+      <div className="p-3">
         Is this your first time here? <span className="text-sky-500 underline hover:cursor-pointer hover:text-sky-600">Create account</span>
       </div>
     </div>
--- NFT-project/src/shared/MainMenu.tsx
+++ NFT-project/src/shared/MainMenu.tsx
@@ -1,14 +1,28 @@
+import { useState } from 'react'
 import './MainMenu.css'
 import { NavLink } from 'react-router'
 
 
 export function MainMenu() {
+  const menuLinks = ['Home', 'My shares', 'Browse'];
+
   return (
-    <div className="font-[Roboto] flex flex-row  justify-between bg-blue-100 mx-[10%] mt-[15px] rounded-[60px] h-[80px] items-center px-[25px]">
-      <div className="main-menu-left-container">Left</div>
+    <div className="flex flex-row  justify-between bg-white mx-[8%]  rounded-[60px] h-[88px] items-center px-[25px] shadow-lg fixed top-7 left-0 right-0">
+      <div className="main-menu-left-container flex items-center gap-[40px]">
+        <NavLink to='/'>
+          <img className='h-[115px] c-sky-200' src="/InvestPoint-logo-with-blacktext-removebg-preview.png" alt="Invest-point-logo" />
+        </NavLink>
+        {menuLinks.map((item) => {
+          return (
+            <NavLink className='main-menu-link' to={`/${item.toLowerCase()}`}>
+              {item}
+            </NavLink>
+          )
+        })}
+      </div>
       <div className="main-menu-right-container">
         <NavLink className='button-primary' to={'/login'}>
-            Login
+          Log in
         </NavLink>
       </div>
     </div>
