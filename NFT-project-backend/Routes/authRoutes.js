c1410e626e9bf3163e2ff92ed9c569257464b2dd
Patrik Simandl <patrik.simandl@seznam.cz>
2025-12-04 12:10:03 +0100

    set up a prisma client and make fucntional backend with db

--- NFT-project-backend/Routes/authRoutes.js
+++ NFT-project-backend/Routes/authRoutes.js
@@ -1,13 +1,62 @@
 import express from "express";
+import db from "../prismaClient.ts";
+import bcrypt from 'bcryptjs'
+import dotenv from 'dotenv'
+
 
 const router = express.Router();
 
-router.post('/register',(req,res)=>{
-  const {email,password} = req.body;
+router.post('/register', async (req, res) => {
+  const { email, password } = req.body;
 
-  res.send('right');
+  if (
+    await db.User.findUnique({
+      where: {
+        email
+      }
+    })
+  ) {
+    return res.send('User already exists');
+  }
 
-  console.log('register a user');
+  const hashedPassword = bcrypt.hashSync(password, 4);
+
+
+  await db.User.create({
+    data: {
+      email,
+      password: hashedPassword
+    }
+  })
+
+
+  res.status(201).send('Register a user');
+
+})
+
+router.post('/login', async (req,res) =>{
+  const {email, password } = req.body;
+  
+
+
+  const user = await db.User.findUnique({
+    where: {
+      email
+    }
+  })
+
+  if(!user){
+    return res.sendStatus(404);
+  }
+
+  const passwordIsValid = bcrypt.compareSync(password,user.password);
+
+
+  if(!passwordIsValid){
+    return res.sendStatus(401);
+  }
+
+  return res.send('Logging in was succesful').status(204);
 })
 
 export default router;
---
+++ NFT-project-backend/prismaClient.ts
@@ -0,0 +1,6 @@
+import { PrismaClient } from '@prisma/client';
+
+const db = new PrismaClient();
+
+export default db;
+//create a prisma client with the video;
